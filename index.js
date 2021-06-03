77bedb1b4e6b9a8e9b87e4ce7410993c27ae2779
devin mitchell <devin.a.mitchell@gmail.com>
2021-06-03 16:13:51 -0600

    WIP

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+const server = express()
+
 console.log("HOWDY")
+console.log("USER----->", process.env.USER)
+console.log("SHELL---->", process.env.SHELL)
+
+if (process.env.NODE_ENV === 'production') {
+    console.log('this means this code is deployed')
+}
+
+const PORT = process.env.PORT || 5000
+console.log('PORT--->', PORT)
+
+server.get('/api', (req, res) => {
+    res.json({ message: `${process.env.COHORT} ROCKS!` })
+})
+
+server.listen(PORT, () => {
+    console.log(`listen on ${PORT}`)
+})
